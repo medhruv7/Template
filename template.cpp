78408019da2f3e8823079e5dc2cfe67e77e4c9b4
medhruv7 <medhruv7@gmail.com>
2019-12-02 15:48:19 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+#define int long long
+#define all(a) a.begin(),a.end()
+#define sz(x) ((int)x.size())
+#define ff first 
+#define ss second 
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<string> vs;
+typedef map<int,int> mii;
+typedef map<char,int> mci;
+typedef vector<vector<int>> vvi;
+typedef vector<pair<int,int>> vpii;
+template<class T>
+istream &operator>>(istream &in, vector<T> &arr){
+	for(auto &x: arr) in >> x;
+	return in;
+}
+
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+}
