e52dc664c3dca8250ba49615b296a8836c12a572
medhruv7 <medhruv7@gmail.com>
2019-12-02 15:47:59 +0530

    Create basic.cpp

---
+++ basic.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vvi vector<vector<int>> 
+#define vi vector<int> 
+#define vpii vector<pair<int,int>> 
+#define ff first
+#define ss second
+#define all(x) x.begin(),x.end()
+#define ffor(i,n) for(int i = 0; i < n; ++i)
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+}
